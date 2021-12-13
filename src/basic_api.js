9ca5aed5d71f28383727ab108b323782ea4059ac
Chetan-Goyal <harshrock616@gmail.com>
2021-12-13 19:33:19 +0530

    Adds Query Implementation in Basic API

--- src/basic_api.js
+++ src/basic_api.js
@@ -8,13 +8,30 @@ const app = express()
 const LOCALHOST_PORT = 5000
 
 app.get('/api/products', (req, res) => {
-  res.json(products)
+  const { title, limit } = req.query
+
+  let result = [...products]
+
+  if (title) {
+    result = result.filter((product) => {
+      return product.title.startsWith(title)
+    })
+  }
+
+  if (limit) {
+    result = result.slice(0, Number(limit))
+  }
+
+  if (result.length < 1)
+    return res.json({ "success": true, data: [] })
+
+  return res.json(result)
 })
 
 app.get('/api/products/:productID', (req, res) => {
   const singleProduct = products.find((product) => product.id == Number(req.params['productID']))
   if (!singleProduct) {
-    res.json({})
+    return res.json({})
   }
   res.json(singleProduct)
 })
